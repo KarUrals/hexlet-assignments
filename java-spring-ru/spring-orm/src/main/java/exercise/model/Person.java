581ef86139f62e039fe3c6b9a4da1ed642314f22
@hexlet/cli <support@hexlet.io>
2023-01-14 17:03:31 +0200

    submit java-spring-ru/spring-orm

--- java-spring-ru/spring-orm/src/main/java/exercise/model/Person.java
+++ java-spring-ru/spring-orm/src/main/java/exercise/model/Person.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 
 // END
 @Entity
+@Getter
+@Setter
 public class Person {
 
     @Id
@@ -20,30 +22,5 @@ public class Person {
     // BEGIN
     private String firstName;
     private String lastName;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     // END
 }
